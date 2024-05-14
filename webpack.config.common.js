f1936916f97d1b27512644150a461cf0ff7e3e15
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-14 15:40:19 +0300

    feat(webpack):Add TypeScript
    Подробнее см документацию

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -11,6 +11,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
@@ -50,6 +53,11 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
 
         type: 'asset/resource',
+      },
+      {
+        test: /\.[tj]sx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
