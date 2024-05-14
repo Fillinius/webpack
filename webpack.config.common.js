eab32f50fc32d4d809b908268a3e2170766b2a44
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-14 12:54:48 +0300

    feat(webpack):fonts
     webpack.config.js
     module.exports = {
       entry: './src/index.js',
       output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
       },
       module: {
         rules: [
              {
            test: /\.(woff|woff2|eot|ttf|otf)$/i,
            type: 'asset/resource',
          },
         ],
       },
     };

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -31,6 +31,11 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+      },
     ],
   },
 }
