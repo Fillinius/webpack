67de6ea4ee23d77f79d23eb4a6b8437974fb3457
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-15 10:53:12 +0300

    feat(webpack):Add webpack-bundle-analizer
    сборка на прод
    npm run build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,20 @@
 const { merge } = require('webpack-merge')
-const commonConfig = require('./webpack.config.common')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const commonConfig = require('./webpack.config.common')
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'report.html',
+      openAnalyzer: false,
+    }),
+  ],
   optimization: {
     minimize: true,
     minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
